92c84ba23a641e68167417cd414227a65ded9aec
akings75 <ahmetakings75@gmail.com>
2023-04-14 19:29:57 +0300

    Initial Commit

---
+++ Ricky&MortinApi/Ricky_MortinApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ricky_MortinApiApp.swift
+//  Ricky&MortinApi
+//
+//  Created by Ahmet Akın Özbulut on 14.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Ricky_MortinApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
